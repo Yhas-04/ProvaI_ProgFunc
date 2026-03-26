79f0af33944baa75394e551b4fabc61a0e4d5fc9
Yhas-04 <yhasmin.com.br@gmail.com>
2026-03-26 09:44:32 -0300

    adicionando console.log

--- pipelines.js
+++ pipelines.js
@@ -36,3 +36,21 @@ const vendasEmFotografiaEAudio = pipe(
 );
 
 export { pipe, filtrarPorCategorias, topProdutos, vendasEmFotografiaEAudio };
+
+  const resultado1 = pipe(x => x+1)(10);
+  console.log("\npipe usando apenas reduce:", resultado1)
+  console.log("_____________________________________")
+
+  const resultado2 = filtrarPorCategorias(["móveis", "fotografia"])(vendas)
+  console.log("novo estado\n",resultado2)
+  console.log("\n_____________________________________")
+
+  const resultado3 = topProdutos("móveis")(vendas)
+  console.log("\nPipeline 1: escolham uma análise\n", resultado3)
+  console.log("testee")
+  console.log("_____________________________________")
+
+  const resultado4 = vendasEmFotografiaEAudio(vendas)
+  console.log("\nPipeline 2: uma análise diferente, que use pelo menos um estágio novo\n", resultado4)
+
+                                                  'deveria ter 3 vendas nessas categorias\n\n')
