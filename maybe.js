d4fde15fab72f6321c7cda3d131ea64ae8d35546
Yhas-04 <yhasmin.com.br@gmail.com>
2026-03-26 14:37:28 -0300

    funções maybe e começo html

--- maybe.js
+++ maybe.js
@@ -36,3 +36,32 @@ class Maybe {
       : `Just(${JSON.stringify(this.valor)})`;
   }
 }
+
+const safePipe1 = (...fns) => (valor) =>
+  fns.reduce(
+    (maybe, fn) => maybe.chain(v => Maybe.of(fn(v))),
+    Maybe.of(valor)
+  );
+
+
+const safePipe2 = (categoria, valorMinimo) =>
+  safePipe1(
+    filtrarPorCategoria(categoria),
+    filtrarPorValorMinimo(valorMinimo),
+    totalPorCategoria
+  );
+
+
+if (require.main === module) {
+  const vendas = require('./vendas.json')
+
+  console.log('\ncategoria que existe')
+  const r1 = safePipe2('móveis', 500)(vendas)
+  console.log('resultado:', r1.toString())
+  console.log('valor:', r1.getOrElse({}))
+
+  console.log('\ncategoria que não existe')
+  const r2 = safePipe2('cabos', 500)(vendas)
+  console.log('resultado:', r2.toString())
+  console.log('valor:', r2.getOrElse({}))
+}
